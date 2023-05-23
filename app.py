05df90541ba9d29b0852007acafbe80a86f754f3
AYUSH UPADHYAY <77583924+ayush-u01@users.noreply.github.com>
2023-05-24 02:16:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,6 @@ def draw_all( key, plot=False, ):
         """
         # Fashion Recommendation Web App
         
-        This Web App can do anything u can imagine. 😱 
-        
         This App is built using pretrained transformers which are capable of doing wonders with the images.
         
         ```python
@@ -62,25 +60,25 @@ def main():
             # display the file
             display_image = Image.open(uploaded_file)
             st.image(display_image)
-            # feature extract
-            # features = feature_extraction(os.path.join("uploads",uploaded_file.name),model)
-            #st.text(features)
+            feature extract
+            features = feature_extraction(os.path.join("uploads",uploaded_file.name),model)
+            st.text(features)
             # recommendention
-            # indices = recommend(features,feature_list)
+            indices = recommend(features,feature_list)
             # show
 
-            # col1,col2,col3,col4,col5 = st.beta_columns(5)
+            col1,col2,col3,col4,col5 = st.beta_columns(5)
 
-            # with col1:
-            #     st.image(filenames[indices[0][0]])
-            # with col2:
-            #     st.image(filenames[indices[0][1]])
-            # with col3:
-            #     st.image(filenames[indices[0][2]])
-            # with col4:
-            #     st.image(filenames[indices[0][3]])
-            # with col5:
-            #     st.image(filenames[indices[0][4]])
+            with col1:
+                st.image(filenames[indices[0][0]])
+            with col2:
+                st.image(filenames[indices[0][1]])
+            with col3:
+                st.image(filenames[indices[0][2]])
+            with col4:
+                st.image(filenames[indices[0][3]])
+            with col5:
+                st.image(filenames[indices[0][4]])
         
         
         else:
